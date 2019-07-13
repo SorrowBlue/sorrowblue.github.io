b9572cad655714dfa267759f0c7dc78cf81d823c
SorrowBlue <sorrowblue.dev@gmail.com>
2019-07-14 03:12:52 +0900

    a

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
+	root: true,
+	env: {
+		browser: true,
+		node: true
+	},
+	parserOptions: {
+		parser: 'babel-eslint'
+	},
+	extends: [
     '@nuxtjs',
     'plugin:nuxt/recommended'
   ],
-  // add your custom rules here
-  rules: {
-  }
+	// add your custom rules here
+	rules: {
+		"no-tabs": "off",
+		"vue/html-indent": "off"
+	}
 }
