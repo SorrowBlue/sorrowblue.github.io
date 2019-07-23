d49cfd2884e71a4b2dddfb3e07c877a44c14ee5a
SorrowBlue <sorrowblue.dev@gmail.com>
2019-07-23 16:36:47 +0900

    :tada:

--- pages/blog/_date/_slug/index.vue
+++ pages/blog/_date/_slug/index.vue
@@ -25,7 +25,7 @@ export default {
       meta: [
         { hid: 'og:url', property: 'og:url', content: url },
         { hid: 'og:type', property: 'og:type', content: 'article' },
-        { hid: 'og:image', property: 'og:image', content: 'https://sorrowblue.com/images/ogp/blog.webp' },
+        { hid: 'og:image', property: 'og:image', content: 'https://sorrowblue.com/images/ogp/blog.png' },
         { hid: 'og:title', property: 'og:title', content: title },
         { hid: 'og:site_name', property: 'og:site_name', content: 'sorrowblue.github.io' }
       ],
