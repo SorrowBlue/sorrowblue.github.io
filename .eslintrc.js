8b420adba0e9b8315229ab9790047ad96d201be8
SorrowBlue <sorrowblue.dev@gmail.com>
2019-08-04 14:06:48 +0900

    :tada:

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,6 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
   ],
-  // add your custom rules here
   rules: {
     'arrow-parens': 'off',
     'vue/html-self-closing': 'off',
